e5c5a1f3673ce67700ec787ad6232337c25cd3e6
yanan.huang <yanan78100@163.com>
2019-03-16 12:08:03 +0800

    test update

--- Test.js
+++ Test.js
@@ -43,6 +43,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red'
+    backgroundColor: 'red',
+	padding: 2
   }
 });
